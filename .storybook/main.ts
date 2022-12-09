40a6b881a43552d29282617637321841d10ddc78
Patryk Szydłowski <28259376+patryk-szydlowski@users.noreply.github.com>
2022-12-09 19:29:47 +0100

    this actually works? lol

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+import {StorybookConfig} from "@storybook/html-vite"
+
+const config: StorybookConfig = {
+    stories: ["../src"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions"
+    ],
+    framework: {
+        name: "@storybook/html-vite",
+        options: {}
+    },
+    features: {
+        interactionsDebugger: true
+    },
+    core: {
+        disableTelemetry: true,
+    },
+    docs: {
+        enabled: true
+    }
+}
+
+export default config;
